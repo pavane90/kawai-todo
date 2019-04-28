81208d4d5f5df2e5226cfda7b877ec1276c1fcb3
Lee, Byungman <pavane90@gmail.com>
2019-04-28 18:00:31 +0900

    Coding the UI part Two

--- kawai-todo/App.js
+++ kawai-todo/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from "./ToDo"
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newTodo: ""
+  }
   render() {
+    const { newTodo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"/>
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"} 
+            value = {newTodo}
+            onChangeText={this._crontolNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
   }
+  _crontolNewToDo = text => {
+    this.setState({
+      newTodo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -50,5 +71,11 @@ const styles = StyleSheet.create({
         elevation:3
       }
     })
+  },
+  input: {
+    padding : 20,
+    borderBottomColor : "#bbb",
+    borderBottomWidth : 1,
+    fontSize: 25
   }
 });
---
+++ kawai-todo/ToDo.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native"
+
+export default class ToDo extends React.Component{
+    render(){
+        return(
+        <View>
+            <Text>Hello I'm a ToDo.jS
+            </Text>
+        </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
