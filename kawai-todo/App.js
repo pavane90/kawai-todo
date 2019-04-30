c42334095f52c51f268c49a70a7ba77794e2a811
Lee, Byungman <pavane90@gmail.com>
2019-05-01 01:08:18 +0900

    Styling the To Do Component part Two

--- kawai-todo/App.js
+++ kawai-todo/App.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
-import ToDo from "./ToDo"
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView
+} from "react-native";
+import ToDo from "./ToDo";
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
     newTodo: ""
-  }
+  };
   render() {
     const { newTodo } = this.state;
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content"/>
+        <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput 
-            style={styles.input} 
-            placeholder={"New To Do"} 
-            value = {newTodo}
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newTodo}
             onChangeText={this._crontolNewToDo}
             placeholderTextColor={"#999"}
             returnKeyType={"done"}
             autoCorrect={false}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
-            <ToDo />
+            <ToDo text={"Hello I'm a ToDo"} />
           </ScrollView>
         </View>
       </View>
@@ -41,8 +50,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F23657',
-    alignItems: 'center'
+    backgroundColor: "#F23657",
+    alignItems: "center"
   },
   title: {
     color: "white",
@@ -55,30 +64,30 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     flex: 1,
     width: width - 25,
-    borderTopLeftRadius : 10,
-    borderTopRightRadius : 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
     ...Platform.select({
-      ios : {
-        shadowColor:"rgb(50,50,50)",
+      ios: {
+        shadowColor: "rgb(50,50,50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
-        shadowOffset:{
-          height:1,
-          width:0
+        shadowOffset: {
+          height: 1,
+          width: 0
         }
       },
-      android:{
-        elevation:3
+      android: {
+        elevation: 3
       }
     })
   },
   input: {
-    padding : 20,
-    borderBottomColor : "#bbb",
-    borderBottomWidth : 1,
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
     fontSize: 25
   },
-  toDos:{
-    alignItems:"center"
+  toDos: {
+    alignItems: "center"
   }
 });
